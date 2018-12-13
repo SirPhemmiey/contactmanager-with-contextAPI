524f7d1e15bbcddba2b046590740dc6fd2fc7131
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-13 23:24:41 +0100

    Updates

---
+++ precache-manifest.b90a4e5502c3c59f514318be87c5c0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ce11dfd6de9113709f4",
+    "url": "/contactmanager-with-contextAPI/static/js/runtime~main.7ce11dfd.js"
+  },
+  {
+    "revision": "8c7fbf88c3dd35319199",
+    "url": "/contactmanager-with-contextAPI/static/js/main.8c7fbf88.chunk.js"
+  },
+  {
+    "revision": "1196d18d78cc846c08d1",
+    "url": "/contactmanager-with-contextAPI/static/js/1.1196d18d.chunk.js"
+  },
+  {
+    "revision": "8c7fbf88c3dd35319199",
+    "url": "/contactmanager-with-contextAPI/static/css/main.e4be5b2f.chunk.css"
+  },
+  {
+    "revision": "1196d18d78cc846c08d1",
+    "url": "/contactmanager-with-contextAPI/static/css/1.420042e4.chunk.css"
+  },
+  {
+    "revision": "d96a6660341987a7c2d3d5d59b4258a4",
+    "url": "/contactmanager-with-contextAPI/index.html"
+  }
+];
