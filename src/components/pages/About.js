452dc786078b76994c8accee8588d187a8a97f60
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-13 23:11:51 +0100

    Commit all

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple App to manage Contacts</p>
+      <div className="text secondary">Version 1.0.0</div>
+    </div>
+  )
+}
---
+++ src/components/test/Test.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+
+  state = {
+    title: '',
+    body: ''
+  }
+  // componentDidMount() {
+  //   //console.log("did mount");
+  //   fetch('https://jsonplaceholder.typicode.com/posts/1')
+  // .then(response => response.json())
+  // .then(data => this.setState({
+  //   title: data.title,
+  //   body: data.body
+  // }))
+  // }
+
+  render() {
+    return (
+      <div>
+      <h1>Test Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Test
